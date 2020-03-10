b9f902a7c42b49596c508d61976d48693bbbf4b7
Миронов Андрей <and@smarthead.ru>
2020-03-10 12:11:38 +0300

    Add css modules, scss, staging environment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <div className={styles.app}>
+            <header className={styles.header}>
+                <img src={logo} className={styles.logo} alt="logo" />
                 <p>
                     Edit
                     {' '}
                     <code>src/App.js</code>
                     {' '}
                     and save to reload.
+                    {' '}
+                    {process.env.REACT_APP_TEST_STRING}
                 </p>
                 <a
-                    className="App-link"
+                    className={styles.link}
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
